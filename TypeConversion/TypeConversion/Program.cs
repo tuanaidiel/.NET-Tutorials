08bc881f7bd575d95203a84be29164720ff01422
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-01-08 17:45:07 +0800

    Add files via upload

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            byte b = 1;
+            int i = b;               //can convert byte to int
+            Console.WriteLine(i);
+
+            int e = 1;
+            byte r = (byte) e;       //can't convert int to byte
+            Console.WriteLine(r);
+
+            int t = 1000;            //maximum byte is 255
+            byte y = (byte)t;       
+            Console.WriteLine(y);
+        }
+    }
+}
