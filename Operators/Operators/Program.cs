5c48ff3a6fb22a2d8dca7dc8508cd3084f5fc99d
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-01-08 19:12:08 +0800

    Add files via upload

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+
+            Console.WriteLine(a + b);
+
+            var c = 3;
+            var d = 4;
+            var e = 5;
+
+            Console.WriteLine(e < d || e == c);  //False
+        }
+    }
+}
